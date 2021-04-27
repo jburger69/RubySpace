cfe37e41f8ee9321e7db53ee6ac3e323fd7a1c2f
Joshua Burger <joshberger6969@outlook.com>
2021-04-26 23:42:48 -0400

    New migration for posts table

---
+++ db/migrate/20210425010418_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.text :content
+      t.string :image_url
+      t.integer :views
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
