031585c48497fa07855baf57dd1d2a76f3f35baf
Joshua Burger <joshberger6969@outlook.com>
2021-04-27 00:01:11 -0400

    validation for username presence and uniqueness, new associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :username, uniqueness: true
+
+  has_many :posts
+  has_many :authors
+  has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :followees, through: :followed_users
+  has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
+  has_many :followers, through: :following_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
 end
