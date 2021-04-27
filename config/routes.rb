3cb010cb45de29ef3b918e3a4b0b7c71780fc1de
Joshua Burger <joshberger6969@outlook.com>
2021-04-27 00:08:47 -0400

    New resources for posts with comments nested, New custom search route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get '/search' => 'application#search', :as => 'search_page'
   
+  devise_for :users
+  resources :posts do
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#welcome'
-  devise_for :users
+  
 end
