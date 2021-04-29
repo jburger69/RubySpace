83d5371f988c30631c5b00cf10afff51b7189642
Joshua Burger <joshberger6969@outlook.com>
2021-04-29 16:19:46 -0400

    Define new create action for comments with strong_params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+
+    def create
+        @post = current_user.posts.find_by_id(params[:post_id])
+        @comment = @post.comments.build(comment_params)
+        if @comment.save
+            flash[:message] = "Comment successful"
+            redirect_to post_path(@post)
+        else
+            flash[:message] = "Sorry that didnt work"
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :post_id, :author_id)
+    end
 end
