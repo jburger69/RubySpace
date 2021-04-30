8df094fa023ed02a52d93b723898cf2f4e03e4ab
Joshua Burger <joshberger6969@outlook.com>
2021-04-29 20:19:52 -0400

    Made change to type :username to :user validates, has_many associations for comments and commented post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :username, uniqueness: true
+  validates :name, presence: true
+  validates :name, uniqueness: true
 
   has_many :posts
-  has_many :authors
+  has_many :comments, foreign_key: :author_id, class_name: "Comment"
+  has_many :commented_posts, through: :comments, source: :author
   has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
   has_many :followees, through: :followed_users
   has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
