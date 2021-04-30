6b7242d0e798a5dfb0fd61ff4d2bb372d9af43b7
Joshua Burger <joshberger6969@outlook.com>
2021-04-29 20:17:53 -0400

    New foreign_key for Author, Scope for order created at desc

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,7 @@ class Comment < ApplicationRecord
 
 
     belongs_to :post
-    belongs_to :author, class_name: 'User'
+    belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+    default_scope { order(created_at: :desc)}
 end
