b977556a3f8657fa8a6d3f26b249968cc8de4dd7
Joshua Burger <joshberger6969@outlook.com>
2021-05-02 16:57:09 -0400

    create new devise omniauthable and provides for facebook, New class method for omniauth, follow method to follow a user, unfollow method to unfollow a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+      devise :omniauthable, :omniauth_providers => [:facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.name = auth.info.name
+      user.password = Devise.friendly_token[0,20]
+    end 
+  end
+
+  def follow(user_id)
+    followed_users.create(followee_id: user_id)
+  end
+
+  def unfollow(user_id)
+    followed_users.find_by(followee_id: user_id).destroy
+  end
 
 
 end
