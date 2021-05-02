39fb9e14e5805bc496718f2ce0ff0bfde3a59a51
Joshua Burger <joshberger6969@outlook.com>
2021-05-02 16:44:45 -0400

    New get route for /most_recent posts, New get routes for followers and following, New devise_for users with omniauth callbacks, resources for users only index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   get '/search' => 'application#search', :as => 'search_page'
+  get '/most_recent', to: 'posts#most_recent', as: 'recent'
+  get '/users/:id/followers' => 'follows#followers'
+  get '/users/:id/following' => 'follows#followees'
+  post '/users/:id/follow_user' => 'follows#follow_user'
+  post '/users/:id/unfollow_user' => 'follows#unfollow_user'
   
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+    
+
+  resources :users, only: [:index, :show]
   resources :posts do
     resources :comments
   end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#welcome'
   
